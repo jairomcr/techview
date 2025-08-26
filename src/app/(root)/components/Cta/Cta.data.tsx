83a481c5b2676ef20cf0b6dc763ba7a368558ebf
Jairo Michel <jairocascaretrodriguez@gmail.com>
2025-08-25 23:15:48 -0400

    feat:cta home

---
+++ src/app/(root)/components/Cta/Cta.data.tsx
@@ -0,0 +1,14 @@
+export const ctaData = [
+    {
+        name:"Developers Trained",
+        value:"10k+" 
+    },
+    {
+        name:"Succeess Rate",
+        value:"94K%+" 
+    },
+    {
+        name:"Companies Hiring",
+        value:"500K+" 
+    },
+]
